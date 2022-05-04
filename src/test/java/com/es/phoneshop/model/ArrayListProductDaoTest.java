5f49e83d31f79edb757cdfb355e07a774fdb693b
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2022-05-04 17:49:13 +0300

    Implement unit tests

--- src/test/java/com/es/phoneshop/model/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/ArrayListProductDaoTest.java
@@ -1,17 +1,22 @@
 package com.es.phoneshop.model;
 
-import com.es.phoneshop.model.product.ArrayListProductDao;
+import com.es.phoneshop.model.product.dao.ArrayListProductDao;
 import com.es.phoneshop.model.product.Product;
-import com.es.phoneshop.model.product.ProductDao;
+import com.es.phoneshop.model.product.dao.ProductDao;
 import com.es.phoneshop.model.product.ProductNotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.Currency;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class ArrayListProductDaoTest
 {
@@ -19,12 +24,12 @@ public class ArrayListProductDaoTest
 
     @Before
     public void setup() throws ProductNotFoundException {
-        productDao = new ArrayListProductDao();
+        productDao = ArrayListProductDao.getInstance();
     }
 
     @Test
     public void testFindProductsNoResults() {
-        assertFalse(productDao.findAll().isEmpty());
+        assertTrue(productDao.findAll().isEmpty());
     }
 
     @Test
@@ -32,8 +37,8 @@ public class ArrayListProductDaoTest
         Product product = createProduct();
         Product result = productDao.findEntityById(product.getId());
 
-        assertNotNull(result);
-        assertEquals("test-product", result.getCode());
+        assertThat(result).isNotNull();
+        assertThat(result.getCode()).isEqualTo("test-product");
     }
 
     @Test
@@ -41,8 +46,8 @@ public class ArrayListProductDaoTest
         Product product = createProduct();
         Product result = productDao.findEntityById(product.getId());
 
-        assertNotNull(result);
-        assertEquals(0, product.getStock());
+        assertThat(result).isNotNull();
+        assertThat(product.getStock()).isZero();
     }
 
     @Test
@@ -52,7 +57,8 @@ public class ArrayListProductDaoTest
         productDao.delete(product);
         List<Product> productList = productDao.findAll();
 
-        assertFalse(productList.contains(product));
+        assertThat(productList).isNotNull()
+                .doesNotContain(product);
     }
 
     @Test
@@ -62,7 +68,50 @@ public class ArrayListProductDaoTest
         productDao.deleteById(product.getId());
         List<Product> productList = productDao.findAll();
 
-        assertFalse(productList.contains(product));
+        assertThat(productList).isNotNull()
+                .doesNotContain(product);
+    }
+
+    @Test
+    public void testFindProducts() {
+        Product product = createProduct();
+        List<Product> result = productDao.findProducts(product.getDescription());
+
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    public void testFindAscDescriptionSortedProducts() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        SortField sortField = SortField.description;
+        SortOrder sortOrder = SortOrder.asc;
+        List<Product> productSortedList = productDao.findAll();
+        Method sortComparator = ArrayListProductDao.class.getDeclaredMethod("sortComparator", SortField.class);
+
+        sortComparator.setAccessible(true);
+
+        Comparator<Product> comparator = (Comparator<Product>) sortComparator.invoke(ArrayListProductDao.class, sortField);
+        productSortedList = productSortedList.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+
+        assertThat(productDao.findSortedProducts(sortField, sortOrder)).isEqualTo(productSortedList);
+    }
+
+    @Test
+    public void testFindDescPriceSortedProducts() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        SortField sortField = SortField.price;
+        SortOrder sortOrder = SortOrder.desc;
+        List<Product> productSortedList = productDao.findAll();
+        Method sortComparator = ArrayListProductDao.class.getDeclaredMethod("sortComparator", SortField.class);
+
+        sortComparator.setAccessible(true);
+
+        Comparator<Product> comparator = (Comparator<Product>) sortComparator.invoke(ArrayListProductDao.class, sortField);
+        productSortedList = productSortedList.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+
+        assertThat(productDao.findSortedProducts(sortField, sortOrder)).isEqualTo(productSortedList);
     }
 
     private Product createProduct() {
@@ -70,7 +119,7 @@ public class ArrayListProductDaoTest
         Product product = new Product("test-product", "Samsung Galaxy S", new BigDecimal(100), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
         productDao.create(product);
 
-        assertNotNull(product.getId());
+        assertThat(product.getId()).isNotNull();
 
         return product;
     }
