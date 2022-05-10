fd4122844e73ca2517777898d54eea1cb9e8fc58
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2022-05-10 21:51:31 +0300

    Task 5.3: primitive DOS protection

---
+++ src/main/java/com/es/phoneshop/security/DefaultDosProtectionService.java
@@ -0,0 +1,36 @@
+package com.es.phoneshop.security;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DefaultDosProtectionService implements DosProtectionService {
+    private static final long THRESHOLD = 10;
+
+    private Map<String, Long> countMap = new ConcurrentHashMap<>();
+
+    private static class SingletonHelper {
+        private static final DefaultDosProtectionService INSTANCE = new DefaultDosProtectionService();
+    }
+
+    public static DefaultDosProtectionService getInstance() {
+        return DefaultDosProtectionService.SingletonHelper.INSTANCE;
+    }
+
+    @Override
+    public boolean isAllowed(String ip) {
+        Long count = countMap.get(ip);
+
+        if(count == null) {
+            count = 1L;
+        } else {
+            if(count > THRESHOLD){
+                return false;
+            }
+
+            count++;
+        }
+        countMap.put(ip, count);
+
+        return true;
+    }
+}
