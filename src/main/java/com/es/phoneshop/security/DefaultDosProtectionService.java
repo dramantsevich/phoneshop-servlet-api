e198cf40a3076f6fbf943766f885710cf1a81a9f
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2022-05-16 13:51:47 +0300

    refactor dos protection service

--- src/main/java/com/es/phoneshop/security/DefaultDosProtectionService.java
+++ src/main/java/com/es/phoneshop/security/DefaultDosProtectionService.java
@@ -1,36 +1,44 @@
 package com.es.phoneshop.security;
 
 import java.util.Map;
+import java.util.Timer;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.TimerTask;
 
 public class DefaultDosProtectionService implements DosProtectionService {
-    private static final long THRESHOLD = 30;
+    private static final long THRESHOLD = 20;
+    private static final DefaultDosProtectionService INSTANCE = new DefaultDosProtectionService();
 
     private Map<String, Long> countMap = new ConcurrentHashMap<>();
 
-    private static class SingletonHelper {
-        private static final DefaultDosProtectionService INSTANCE = new DefaultDosProtectionService();
-    }
-
     public static DefaultDosProtectionService getInstance() {
-        return DefaultDosProtectionService.SingletonHelper.INSTANCE;
+        return INSTANCE;
     }
 
     @Override
     public boolean isAllowed(String ip) {
         Long count = countMap.get(ip);
+        final boolean[] temp = {true};
 
-        if(count == null) {
+        if (count == null) {
             count = 1L;
         } else {
-            if(count > THRESHOLD){
-                return false;
-            }
+            Timer timer = new Timer();
+            Long finalCount = count;
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (finalCount > THRESHOLD) {
+                        temp[0] = false;
+                    }
+                }
+            }, 0, 60 * 1000);
 
             count++;
         }
         countMap.put(ip, count);
 
-        return true;
+        return temp[0];
     }
 }
--- src/main/java/com/es/phoneshop/web/DosFilter.java
+++ src/main/java/com/es/phoneshop/web/DosFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class DosFilter implements Filter{
+public class DosFilter implements Filter {
     private DosProtectionService dosProtectionService;
 
     @Override
@@ -17,7 +17,7 @@ public class DosFilter implements Filter{
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if(dosProtectionService.isAllowed(request.getRemoteAddr())) {
+        if (dosProtectionService.isAllowed(request.getRemoteAddr())) {
             chain.doFilter(request, response);
         } else {
             ((HttpServletResponse) response).setStatus(429);
