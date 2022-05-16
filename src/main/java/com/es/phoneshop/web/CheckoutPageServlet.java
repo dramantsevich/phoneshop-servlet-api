3959608d6620f9a685da318202988832f9056b52
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2022-05-16 13:51:29 +0300

    refactor checkout page servlet

--- src/main/java/com/es/phoneshop/web/CheckoutPageServlet.java
+++ src/main/java/com/es/phoneshop/web/CheckoutPageServlet.java
@@ -1,13 +1,7 @@
 package com.es.phoneshop.web;
 
-import com.es.phoneshop.model.cart.Cart;
-import com.es.phoneshop.model.cart.CartService;
-import com.es.phoneshop.model.cart.DefaultCartService;
-import com.es.phoneshop.model.order.DefaultOrderService;
-import com.es.phoneshop.model.order.Order;
-import com.es.phoneshop.model.order.OrderService;
-import com.es.phoneshop.model.order.PaymentMethod;
-import com.es.phoneshop.model.product.ProductNotFoundException;
+import com.es.phoneshop.model.cart.*;
+import com.es.phoneshop.model.order.*;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -15,31 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
 
 public class CheckoutPageServlet extends HttpServlet {
-    private CartService cartService;
-    private OrderService orderService;
+    private CartDao cartDao;
+    private OrderDao orderDao;
+    private DefaultOrderService orderService;
 
     @Override
-    public void init(ServletConfig config) throws  ServletException{
-        super.init(config);
-        try {
-            cartService = DefaultCartService.getInstance();
-            orderService = DefaultOrderService.getInstance();
-        } catch (ProductNotFoundException e) {
-            e.printStackTrace();
-        }
+    public void init(ServletConfig config) {
+        cartDao = ArrayListCartDao.getInstance();
+        orderDao = ArrayListOrderDao.getInstance();
+        orderService = DefaultOrderService.getInstance();
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Cart cart = cartService.getCart(request);
-        request.setAttribute("order", orderService.getOrder(cart));
+        Cart cart = cartDao.getCart(request);
+        request.setAttribute("order", orderDao.getOrder(cart));
         request.setAttribute("paymentMethods", orderService.getPaymentMethod());
 
         request.getRequestDispatcher("/WEB-INF/pages/checkout.jsp").forward(request, response);
@@ -47,66 +35,18 @@ public class CheckoutPageServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Cart cart = cartService.getCart(request);
-        Order order = orderService.getOrder(cart);
+        Cart cart = cartDao.getCart(request);
+        Order order = orderDao.getOrder(cart);
         Map<String, String> errors = new HashMap<>();
 
-        setRequiredParameter(request, "firstName", errors, order::setFirstName);
-        setRequiredParameter(request, "lastName", errors, order::setLastName);
-        setRequiredParameter(request, "phone", errors, order::setPhone);
-        setDeliveryDate(request, errors, order);
-        setRequiredParameter(request, "deliveryAddress", errors, order::setDeliveryAddress);
-        setPaymentMethod(request, errors, order);
+        orderService.setRequiredParameter(request, "firstName", errors, order::setFirstName);
+        orderService.setRequiredParameter(request, "lastName", errors, order::setLastName);
+        orderService.setRequiredParameter(request, "phone", errors, order::setPhone);
+        orderService.setDeliveryDate(request, errors, order);
+        orderService.setRequiredParameter(request, "deliveryAddress", errors, order::setDeliveryAddress);
+        orderService.setPaymentMethod(request, errors, order);
 
-        handleError(request, response, errors, order);
+        HandleError.handleCheckoutError(request, response, errors, order);
     }
 
-    private void handleError(HttpServletRequest request, HttpServletResponse response, Map<String, String> errors,
-                             Order order) throws IOException, ServletException {
-        Cart cart = cartService.getCart(request);
-
-        if(errors.isEmpty()) {
-            orderService.placeOrder(order);
-            cartService.clearCart(cart);
-            response.sendRedirect(request.getContextPath() + "/order/overview/" + order.getSecureId());
-        } else {
-            request.setAttribute("cart", cart);
-            request.setAttribute("errors", errors);
-            request.setAttribute("order", order);
-            request.setAttribute("paymentMethods", orderService.getPaymentMethod());
-            request.getRequestDispatcher("/WEB-INF/pages/checkout.jsp").forward(request, response);
-        }
-    }
-
-    private void setRequiredParameter(HttpServletRequest request, String parameter, Map<String, String> errors, Consumer<String> consumer) {
-        String value = request.getParameter(parameter);
-
-        if(value == null || value.isEmpty()) {
-            errors.put(parameter, "Value is required");
-        } else {
-            consumer.accept(value);
-        }
-    }
-
-    private void setDeliveryDate(HttpServletRequest request, Map<String, String> errors, Order order) {
-        String value = request.getParameter("deliveryDate");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/dd/yyyy")
-                .withLocale(request.getLocale());
-
-        if(value == null || value.isEmpty()) {
-            errors.put("deliveryDate", "Value is required");
-        } else {
-            order.setDeliveryDate(LocalDate.parse(value, formatter));
-        }
-    }
-
-    private void setPaymentMethod(HttpServletRequest request, Map<String, String> errors, Order order) {
-        String value = request.getParameter("paymentMethod");
-
-        if(value == null || value.isEmpty()) {
-            errors.put("paymentMethod", "Value is required");
-        } else {
-            order.setPaymentMethod(PaymentMethod.valueOf(value));
-        }
-    }
 }
