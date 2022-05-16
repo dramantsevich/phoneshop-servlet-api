3a0f0b6016ac4768c7c6834adf3f4bb301f9b1ee
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2022-05-16 13:46:36 +0300

    Implement genericDao

--- src/main/java/com/es/phoneshop/model/GenericDao.java
+++ src/main/java/com/es/phoneshop/model/GenericDao.java
@@ -1,6 +1,7 @@
 package com.es.phoneshop.model;
 
-public abstract class GenericDao <T, K> {
-    public abstract T getItem(K id);
-    public abstract void save(T item);
+public interface GenericDao<T, K> {
+    T getItem(K id);
+
+    void save(T item);
 }
